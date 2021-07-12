e429e21057d114cafe2dae3db517b0c3b0787185
grace <grace.c.youngblood@gmail.com>
2021-07-12 19:39:17 -0400

    Display a React component on screen.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show # rename 'display'
+    region = params.permit(:region)
+    json = `tree -Jifs #{Rails.root.join("places", region)}`
+    render json: json
+  end
+end
--- src/packs/application.js
+++ src/packs/application.js
@@ -8,6 +8,29 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const Hello = props => (
+  <div style={{ margin: '2rem' }} >Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: PropTypes.string
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
