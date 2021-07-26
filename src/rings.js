af9def7dc921508ff1d980aba8bd12d0598c3a91
grace <grace.c.youngblood@gmail.com>
2021-07-26 08:28:59 -0400

    Zoom on button click.

--- src/rings.js
+++ src/rings.js
@@ -17,18 +17,40 @@ class Rings extends React.Component {
 
     var size = 0.98 * Math.min(paper.view.center.x, paper.view.center.y)
     place_subring_layer(paper, paper.view.center, size, this.props.code[0])
+
+    this.paper = paper
   }
 
   render = () => (
-    <canvas
-    style={{
-      width: "80vw",
-      height: "80vh",
-      border: "1px solid black",
-      display: "block",
-    }}
-    ref={this.canvas}
-    />
+    <>
+      <canvas
+      style={{
+        width: "80vw",
+        height: "80vh",
+        border: "1px solid black",
+        display: "block",
+      }}
+      ref={this.canvas}
+      />
+
+      <button onClick={() => {
+        var background = new paper.Path.Rectangle(
+          paper.view.bounds
+        )
+        background.fillColor = "#f8f6bb"
+
+        var size = 0.98 * Math.min(this.paper.view.center.x, this.paper.view.center.y)
+        place_subring_layer(
+          this.paper,
+          this.paper.view.center,
+          size,
+          this.props.code[0].contents[11],
+        )
+      }}
+      >
+        Zoom
+      </button>
+    </>
   )
 }
 
